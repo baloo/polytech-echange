3f7919c55838dd2101c3c02731b326b80b8b74fd
superbaloo <superbaloo@Baloo.localdomain>
2009-03-22 16:14:40 +0100

    Fix des sessions

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 ActionController::Routing::Routes.draw do |map|
   map.resources :users
 
-  map.resource :session
+  map.resource :sessions
 
   map.resources :announcements
   
   map.signup '/signup', :controller => 'users', :action => 'new'
-  map.login '/login', :controller => 'session', :action => 'new'
-  map.logout '/logout', :controller => 'session', :action => 'destroy'
+  map.login '/login', :controller => 'sessions', :action => 'new'
+  map.logout '/logout', :controller => 'sessions', :action => 'destroy'
 
   # The priority is based upon order of creation: first created -> highest priority.
 
