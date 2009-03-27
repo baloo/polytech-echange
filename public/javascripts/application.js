e95bb17c8b1b4fb9325d79d322da34e265bebd75
Arthur Gautier <superbaloo@superbaloo.net>
2009-03-27 20:08:38 +0100

    Ajout de l'ajax

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,20 @@
-// Place your application-specific JavaScript functions and classes here
+// vim: ts=2 sw=2 expandtab:
+//// Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+
+
+$(document).ready(function()
+{
+  $('.commenttitle').each(function(){
+    $(this).click(function(){
+      if($(this).hasClass('closed')){
+        $(this).parent().find("ul").eq(0).slideDown(400);
+        $(this).removeClass('closed');
+      }else{
+        $(this).parent().find("ul").eq(0).slideUp(400);
+        $(this).addClass('closed');
+      }
+      return false;
+    });
+  });
+});
